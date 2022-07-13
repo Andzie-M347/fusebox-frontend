4f96ef3b3594972dc3121be5dd810e485b05907d
Andzisi Mabaso <andzisishine@gmail.com>
2022-07-13 04:26:03 +0200

    feat: added more sass files

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Login = () => {
     return (
         <div>
             <h4> Login </h4>
+            <Link to="/dashboard">Dashboard</Link>
         </div>
     )
 }
