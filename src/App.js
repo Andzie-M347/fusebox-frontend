58bf2e67a5a4e7eaa98f34c079a109e32111a8f6
Andzisi Mabaso <andzisishine@gmail.com>
2022-07-13 10:15:54 +0200

    feat: login screen layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import '@/assets/main.scss'
+
 import { Login } from '@/pages/auth/login'
 import { Dashboard } from '@/pages/dashboard'
 import { PrivateRoutes } from '@/routes/privateRoutes'
--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -1,11 +1,62 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { MdOutlineRemoveRedEye } from 'react-icons/md'
+import { FaFacebookF, FaTwitter, FaPinterest } from 'react-icons/fa'
+import Image from '@/assets/images/illustration.png'
 
 export const Login = () => {
     return (
-        <div>
-            <h4> Login </h4>
-            <Link to="/dashboard">Dashboard</Link>
+        <div className="login-screen">
+            <div className="login-screen__left">
+                <img src={Image} alt="Login" />
+            </div>
+            <div className="login-screen__right">
+                <header className="login-screen__left--header">
+                    <h2> Welcome Back!</h2>
+                    <p>Login to your account</p>
+                </header>
+                <form action="">
+                    <div className="form-group">
+                        <label for="email"> Email</label>
+                        <input
+                            type="text"
+                            placeholder="example@gmail.com"
+                            className="form-control"
+                        />
+                    </div>
+                    <div className="form-group  password">
+                        <label for="password"> Password</label>
+                        <input
+                            type="text"
+                            placeholder="8+ characters"
+                            className="form-control"
+                        />
+
+                        <MdOutlineRemoveRedEye />
+                    </div>
+
+                    <div className="form-group">
+                        <button
+                            type="submit"
+                            className="button button__primary">
+                            Login
+                        </button>
+                    </div>
+                </form>
+
+                <footer className="login-screen__footer">
+                    Or login using icons
+                    <span className="footer-screen__footer--icon">
+                        <FaFacebookF />
+                    </span>
+                    <span className="footer-screen__footer--icon">
+                        <FaTwitter />
+                    </span>
+                    <span className="footer-screen__footer--icon">
+                        <FaPinterest />
+                    </span>
+                </footer>
+            </div>
         </div>
     )
 }
