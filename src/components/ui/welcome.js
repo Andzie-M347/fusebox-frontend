27aebb9038cac683c17f53b2f81ae9cf4f44214c
Andzisi Mabaso <andzisishine@gmail.com>
2022-07-18 02:22:04 +0200

    feat: finished welcome message

--- src/components/ui/welcome.js
+++ src/components/ui/welcome.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Container } from './container'
 import { useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { Greeting } from '@/utils'
 
 export const Welcome = () => {
     const navigate = useNavigate()
+
+    const { user } = useSelector(state => state.user)
 
     return (
         <Container>
             <section className="welcome">
                 <div className="welcome__header">
                     <h3 className="title">
-                        <span className="title__main"> Good Morning,</span>
-                        <span className="title__sub"> {'username'}</span>
+                        <span className="title__main"> {Greeting()}</span>
+                        <span className="title__sub"> {`${user?.name}`}</span>
                     </h3>
 
                     <p> Welcome to Donotpanic system</p>
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,3 +5,17 @@ export const formatDate = date => {
     var dd = date.getDate()
     return String(10000 * yyyy + 100 * mm + dd) // Leading zeros for mm and dd
 }
+
+// Greeting based on time
+
+export const Greeting = () => {
+    var myDate = new Date()
+    var hours = myDate.getHours()
+    var greet
+
+    if (hours < 12) greet = 'morning'
+    else if (hours >= 12 && hours <= 17) greet = 'afternoon'
+    else if (hours >= 17 && hours <= 24) greet = 'evening'
+
+    return <span>Good {greet}, </span>
+}
